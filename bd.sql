88e4c1bc02b19205d0ca7db450865ee49c55a743
Igor Pereira <igorp9971@gmail.com>
2017-11-08 10:10:32 -0200

    Script sql bd

---
+++ bd.sql
@@ -0,0 +1,16 @@
+CREATE TABLE base (
+  INT Id,
+  VARCHAR palavra,
+  VARCHAR idioma,
+  VARCHAR contexto,
+  INT equivalente,
+  VARCHAR catGram,
+  VARCHAR exemplo,
+  VARCHAR traducao,
+  VARCHAR referencia
+)
+CREATE TABLE usr (
+  VARCHAR usr,
+  VARCHAR pass,
+  INT nivel_permissao
+)
