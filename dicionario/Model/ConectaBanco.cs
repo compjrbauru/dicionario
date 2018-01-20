917bc48d277d6ac2f1a1250e1d13cdf8c8d5966f
Igor Pereira <igorp9971@gmail.com>
2018-01-20 20:46:36 -0200

    Inicio do desenvolvimento MVC, somente banco e tabela usr

---
+++ dicionario/Model/ConectaBanco.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dicionario.Model
+{
+    class ConectaBanco
+    {
+        private string servidor;
+        private string bancoDados;
+        private string usuario;
+        private string senha;
+        private MySqlConnection conexao;
+
+        public ConectaBanco(string bd, string usr, string pss, string svr = "localhost")
+        {
+            PreparaConexao(bd, usr, pss, svr);
+            servidor = svr;
+            bancoDados = bd;
+            usuario = usr;
+            senha = pss;
+        }
+        private void PreparaConexao(string bd, string usr, string pss, string svr = "localhost")
+        {
+            string connectionString = "SERVER=" + svr + ";" + "DATABASE=" + bd + ";" + "UID=" + usr + ";" + "PASSWORD=" + pss + ";";
+            conexao = new MySqlConnection(connectionString);
+        }
+        private bool AbreConexao()
+        {
+            try
+            {
+                conexao.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                {
+                    switch (ex.ErrorCode)
+                    {
+                        case 0:
+                            MessageBox.Show("Falha ao conectar no servidor de dados.");
+                            break;
+                        case 1045:
+                            MessageBox.Show("A combinacao de usuario e senha nao existe. Tente novamente.");
+                            break;
+                    }
+                }
+                return false;
+            }
+        }
+        private bool FechaConexao()
+        {
+            try
+            {
+                conexao.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+        public void InsereLinha(string tabela, List<string> campos, List<string> valores)
+        {
+            string query = "INSERT INTO " + tabela + " (";
+            foreach (string item in campos)
+            {
+                query += item;
+                query += ", ";
+            }
+            query = query.Remove(query.Length - 2);
+            query += ") VALUES(";
+            foreach (string item in valores)
+            {
+                query += item;
+                query += ", ";
+            }
+            query = query.Remove(query.Length - 2);
+            query += ")";
+            if (this.AbreConexao() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conexao);
+                cmd.ExecuteNonQuery();
+                this.FechaConexao();
+            }
+        }
+        public void UpdateLine(string tabela, List<string> campos, List<string> valores, string filtro = "")
+        {
+            string query = "UPDATE " + tabela + " SET ";
+            string temp1, temp2;
+            while (campos.Count > 0)
+            {
+                temp1 = campos.First();
+                temp2 = valores.First();
+                query += temp1 + "=" + "'" + temp2 + "'";
+                campos.RemoveAt(0);
+                valores.RemoveAt(0);
+                if (campos.Count > 0)
+                    query += ", ";
+            }
+            if (filtro != "")
+                query += "WHERE " + filtro;
+            if (this.AbreConexao() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = query;
+                cmd.Connection = conexao;
+                cmd.ExecuteNonQuery();
+                this.FechaConexao();
+            }
+        }
+        public void ApagaLinha(string tabela, string filtro)
+        {
+            string query = "DELETE FROM " + tabela + " WHERE " + filtro;
+            if (this.AbreConexao() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conexao);
+                cmd.ExecuteNonQuery();
+                this.FechaConexao();
+            }
+        }
+        public List<string>[] Select(string tabela, List<string> campos, string filtro = "", string outrosParam = "")
+        {
+            string query = "SELECT * FROM " + tabela;
+            int qtReg = campos.Count;
+            if (filtro != "")
+                query += " WHERE " + filtro;
+            if (outrosParam != "")
+                query += " " + outrosParam;
+            List<string>[] list = new List<string>[qtReg];
+            for (int i = 0; i < qtReg; i++)
+            {
+                list[i] = new List<string>();
+            }
+            if (this.AbreConexao() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conexao);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                switch (tabela)
+                {
+                    case "usr":
+                        while (dataReader.Read())
+                        {
+                            list[0].Add(dataReader["usr"] + "");
+                            list[1].Add(dataReader["pass"] + "");
+                            list[2].Add(dataReader["nivel_permissao"] + "");
+                            list[3].Add(dataReader["email"] + "");
+                            list[4].Add(dataReader["nome"] + "");
+                            list[5].Add(dataReader["contato"] + "");
+                            list[6].Add(dataReader["rede_social"] + "");
+                            list[7].Add(dataReader["cpf"] + "");
+                            list[8].Add(dataReader["telefone"] + "");
+                        }
+                        break;
+                }
+                dataReader.Close();
+                this.FechaConexao();
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+    }
+}
---
+++ dicionario/Model/Usuario.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dicionario.Model
+{
+    class Usuario
+    {
+        public Usuario() { }
+        public int usr { get; set; }
+        public string pass { get; set; }
+        public string permissao { get; set; }
+        public string email { get; set; }
+        public string nome { get; set; }
+        public string contato { get; set; }
+        public string rsocial { get; set; }
+        public string cpf { get; set; }
+        public string tel { get; set; }
+
+        public override string ToString()
+        {
+            string expressao = "usr=" + usr + ",pass='" + pass + "',nivel_permissao='" + permissao + "',email='" + email + "',nome='" + nome + "',contato='" + contato + "',rede_social='" + rsocial + "',cpf='" + cpf + "',telefone='" + tel;
+            return expressao;
+        }
+
+        public List<string> ToListTabela(bool incluiId = false)
+        {
+            List<string> val = new List<string>();
+            if (incluiId)
+                val.Add("usr");
+            val.Add("pass");
+            val.Add("nivel_permissao");
+            val.Add("email");
+            val.Add("nome");
+            val.Add("contato");
+            val.Add("rede_social");
+            val.Add("cpf");
+            val.Add("telefone");
+            return val;
+        }
+        public List<string> ToListValores(bool incluiId = false)
+        {
+            List<string> val = new List<string>();
+            if (incluiId)
+                val.Add(usr.ToString());
+            val.Add(pass);
+            val.Add(permissao);
+            val.Add(email);
+            val.Add(nome);
+            val.Add(contato);
+            val.Add(rsocial);
+            val.Add(cpf);
+            val.Add(tel);
+            return val;
+        }
+    }
+}
